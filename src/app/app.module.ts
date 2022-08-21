c88392a284ecf333973cea22e787044da1c475ff
EtienneDeziel <enneiteleized@hotmail.com>
2022-08-21 15:08:48 -0400

    Premier Test Angular
    2022-08-21 premier jet d'architecture Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EquipeComponent } from './equipe/equipe.component';
+import { NavComponent } from './section/nav/nav.component';
+import { HeaderTopComponent } from './section/header-top/header-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquipeComponent,
+    NavComponent,
+    HeaderTopComponent
   ],
   imports: [
     BrowserModule,
