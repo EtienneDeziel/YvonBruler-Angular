e2fc371245fb4b36c8f28dd365a08a42879d1d38
EtienneDeziel <enneiteleized@hotmail.com>
2022-08-25 11:58:03 -0400

    commit2
    commit2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EquipeComponent } from './equipe/equipe.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:HomeComponent},
+  { path: 'equipe', component:EquipeComponent },
+  { path: 'menu', component:MenuComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {  HttpClientModule  } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EquipeComponent } from './equipe/equipe.component';
 import { NavComponent } from './section/nav/nav.component';
 import { HeaderTopComponent } from './section/header-top/header-top.component';
+import { FooterComponent } from './section/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EquipeComponent,
     NavComponent,
-    HeaderTopComponent
+    HeaderTopComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
